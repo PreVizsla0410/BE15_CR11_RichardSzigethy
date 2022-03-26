0d19a40e9e5866b49d9f281cf32922e41593c062
Richard Szigehty <91049248+PreVizsla0410@users.noreply.github.com>
2022-03-26 14:59:15 +0100

    Delete components directory

--- components/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="d-flex  bg-light flex-column  align-items-center ">
-    <a href="index.php" class="text-dark text-decoration-none"><h5>Pet Adoption Richard Szigethy CR11&#174; CR11</h5></a>
-</div>
