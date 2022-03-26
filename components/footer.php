562f9f2b5b1b49a6edabdf8a3d3561e50aa01b99
Richard Szigehty <91049248+PreVizsla0410@users.noreply.github.com>
2022-03-26 16:02:52 +0100

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,3 @@
+<div class="d-flex  bg-success flex-column  align-items-center ">
+    <a href="index.php" class="text-dark text-decoration-none"><h5>Pet Adoption Richard Szigethy CR11&#174; CR11</h5></a>
+</div>
---
+++ components/navigation.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+<div class="container-fluid">
+<a class="navbar-brand" href="home.php">Pet Adoption</a>
+</nav>
+</div>
+
+<div class="container">
+</div>
+
+
+
+
